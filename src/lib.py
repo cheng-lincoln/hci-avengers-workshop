cd6942abae4e118e510a9b873271563156ef3fd6
lc <42402065+lincoln-ch@users.noreply.github.com>
2019-05-09 10:38:16 +0200

    fixed raw_input(). updated exercises description.
    updated exercises description. fixed raw_input()'s forward incompatibility

--- src/lib.py
+++ src/lib.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 
 def sha1_digest(s):
     m = hashlib.sha1()
@@ -11,7 +12,10 @@ def next_alphabet(c):
 
 def input_multiple_choice(question, choices):
     q = '{0} ({1}): '.format(question, '/'.join(choices))
-    response = raw_input(q)
+    response = vc_input(q)
     while response not in choices:
-        response = raw_input(q)
+        response = vc_input(q)
     return response
+
+def vc_input(params):
+  return raw_input(params) if (sys.version_info.major <= 2) else input(params)
